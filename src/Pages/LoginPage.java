0d23b26a2331447b0a6ed43afc83487868ee6971
soumyapenu <ssdovepair@gmail.com>
2021-11-09 20:15:12 +0530

    Chages made in login, hometest deleted and searchpage added

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -5,6 +5,11 @@ public class LoginPage {
 	public static void main(String args[]) {
 		
 		System.out.println("This is my login page");
+		
+		String un = "Soumyapenu";
+		String pwd = "fjdshfr";
+		
+		System.out.println("Logged in....");
 	}
 
 }
---
+++ src/Pages/SearchPage.java
@@ -0,0 +1,10 @@
+package Pages;
+
+public class SearchPage {
+	
+	public static void main(String args[]) {
+		
+		System.out.print("This is my Search page");
+	}
+
+}

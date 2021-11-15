6e4e9218e0ae042f50761927e124a2b2830f1514
soumyapenu <ssdovepair@gmail.com>
2021-11-15 19:58:35 +0530

    Updated cart page

--- src/Pages/CartPage.java
+++ src/Pages/CartPage.java
@@ -4,6 +4,7 @@ public class CartPage {
 public static void main(String args[]) {
 		
 		System.out.println("This is my home page");
+		System.out.println("This is additional cart feature");
 	}
 
 }
